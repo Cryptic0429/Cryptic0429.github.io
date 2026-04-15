ec80468010becfe3ff2ce580995903637396e619
HC Zeng <zenghc@sjtu.edu.cn>
2026-04-15 19:59:22 +0800

    简洁化更新

---
+++ assets/js/note-access.js
@@ -0,0 +1,14 @@
+window.noteAccessConfig = {
+    'physics-1': {
+        downloadEnabled: true
+    },
+    'physics-2': {
+        downloadEnabled: false
+    },
+    'circuit-honors': {
+        downloadEnabled: false
+    },
+    'analysis-2': {
+        downloadEnabled: false
+    }
+};
